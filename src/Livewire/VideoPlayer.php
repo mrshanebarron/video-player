93ca77c25a7b0efdd1dd7525580733aff36035ea
Shane Barron <shane@Shanes-MacBook-Pro.local>
2025-12-20 23:52:20 -0500

    Add missing public properties to Livewire component

--- src/Livewire/VideoPlayer.php
+++ src/Livewire/VideoPlayer.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class VideoPlayer extends Component
 {
+    public string $src = '';
+    public ?string $poster = null;
+    public bool $autoplay = false;
+    public bool $loop = false;
+    public bool $muted = false;
+    public bool $controls = true;
+    public string $aspectRatio = '16:9';
+
     public function render()
     {
         return view('sb-video-player::livewire.video-player');
